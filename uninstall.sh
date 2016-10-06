9be7a3a90960e4ee3007a248eee055b786302ee4
Pedro <pedrosolbm@gmail.com>
2016-10-06 10:45:30 -0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,3 +2,6 @@
 sudo updatedb			
 sudo rm -vrf $(locate rcss |egrep -v "home")
 sudo rm -vrf $(locate rcsc |egrep -v "home")
+sudo updatedb
+sudo rm -vrf $(locate rcss |egrep -v "home")
+sudo rm -vrf $(locate rcsc |egrep -v "home")
